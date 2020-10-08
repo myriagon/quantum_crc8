befd7bb47a72c4233b8afb981b5b0a1a33e864b3
S Davis <myriagon@sddw.net>
2020-10-08 23:29:06 +0000

    Initial check-in of experiment 11w

---
+++ 11w/exp11w.cmd
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -x  # echo commands
+
+time ./crc8_formulate_problem.py -f 'exp11w.{0:04d}.txt' \
+    -n 1000 \
+    -t 4 \
+    -c 00011111 \
+    -i 00000000 \
+    -d xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx \
+       xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx \
+       xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx \
+       xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx xxxxxxxx
+
